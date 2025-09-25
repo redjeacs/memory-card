7b2b78a8521c92e7ce3e4818e82bfe410d365aa8
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-09-25 14:42:17 +0800

    main content structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import "./App.css";
 import Header from "./components/header";
+import Card from "./components/card";
+
+const test = { src: "./assets/pokemon-logo.png", name: "test" };
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <div className="cards">
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+          <Card character={test} />
+        </div>
+      </main>
     </>
   );
 }
